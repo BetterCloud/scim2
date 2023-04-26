bd8b6063d125c26de7e0b76492cf813b98b1af2a
Gnongo Patrick BEUGRE <patrickbeugre5450@gmail.com>
2023-04-26 16:21:39 +0200

    feat(TPSVC-20228): migrate to SB3 (#24)

--- spring-boot-starter-scim2/src/main/java/com/bettercloud/scim2/server/config/Scim2Properties.java
+++ spring-boot-starter-scim2/src/main/java/com/bettercloud/scim2/server/config/Scim2Properties.java
@@ -13,6 +13,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.net.URI;
@@ -28,6 +29,7 @@ import jakarta.validation.constraints.NotNull;
 @NoArgsConstructor
 @AllArgsConstructor
 @Slf4j
+@Component
 @ConfigurationProperties(prefix = "scim2")
 public class Scim2Properties {
 
